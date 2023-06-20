a04bed7279ff12caffc8274d76d1598a854cb91b
Shashwat_mahajan <shashwatm.cs.21@nitj.ac.in>
2023-06-20 16:25:14 +0530

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <header className='banner' style={{
+        backgroundSize:"cover",
+        backgroundImage:"url('https://wallpapercave.com/wp/wp1917118.jpg')",
+        backgroundPosition:"center center"
+    }}>
+
+    </header>
+  )
+}
+
+export default Banner
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCIxMAvnVJjQ1_4BYs8Sb5HC7XWkrBGoeQ",
+  authDomain: "netflix-e8fac.firebaseapp.com",
+  projectId: "netflix-e8fac",
+  storageBucket: "netflix-e8fac.appspot.com",
+  messagingSenderId: "264937166618",
+  appId: "1:264937166618:web:ec06b13dfddb12ccea7ea3"
+};
